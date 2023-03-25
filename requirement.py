ecf78b57b251ab78b8fc9e9c214d72e00766bc95
AmandaLihua <60131013+AmandaLihua@users.noreply.github.com>
2023-03-25 16:36:52 +0100

    Add files via upload

--- requirement.py
+++ requirement.py
@@ -1,18 +1,11 @@
 import subprocess
 
-def check_module(module):
-    try:
-        __import__(module)
-        print(f"{module} est déjà présent sur votre environnement")
-    except ImportError:
-        print(f"{module} n'est pas sur votre environnement")
-        install_module(module)
 
 def install_module(module):
     subprocess.check_call(["pip", "install", module])
     print(f"{module} a été installé")
 
-modules = ["os", "datetime", "sys", "nmap", "fpdf", "pyfiglet", "termcolor", "google", "whois", "shodan", "requests", "beautifulsoup4"]
+modules = ["os", "datetime", "sys", "python-nmap", "fpdf", "pyfiglet", "termcolor", "google", "whois", "shodan", "requests", "beautifulsoup4"]
 
 for module in modules:
-    check_module(module)
+    install_module(module)
